4f5ba1e4df91eb9f0d4f9370af7a203d695d2357
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2026-01-24 15:18:06 +0530

    Time: 7 ms (65.22%) | Memory: 47.1 MB (18.38%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,15 +1,21 @@
 class Solution {
-    public int exp(int n, int pos, int k, int nums[]) {
+    static int sum;
+    public int exp(int n, int res,int k, int nums[], int dp[][]) {
         if (n == 0) {
-            if (k == 0) return 1;
+            if (res == k) return 1;
             else return 0;
         }
-        return exp(n - 1, 0, k - nums[n - 1], nums) +
-         exp(n - 1, 1, k + nums[n - 1], nums);
+        if(dp[n][res+sum]!=-1) return dp[n][res+sum];
+        return dp[n][res+sum] = exp(n - 1, res - nums[n - 1], k, nums, dp) +
+         exp(n - 1, res + nums[n - 1], k, nums, dp);
 
     }
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-        return exp(n, 1, target, nums);
+        sum = 0;
+        for(int a:nums) sum += a;
+        int dp[][] = new int[n+1][2*sum+1];
+        for(int a[]:dp) Arrays.fill(a,-1);
+        return exp(n, 0, target, nums, dp);
     }
 }
