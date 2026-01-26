214eef4b94d43fafe594decba5427ca0a9d4b6ae
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2026-01-26 13:36:50 +0530

    Time: 17 ms (61.56%) | Memory: 45.2 MB (69.38%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.java
+++ 368-largest-divisible-subset/largest-divisible-subset.java
@@ -3,27 +3,25 @@ class Solution {
         int n = nums.length;
         Arrays.sort(nums);
         int prev[] = new int[n];
-        int group[] = new int[n];
-        int maxIn = 0;
-        for (int i = 0; i < n; i++) {
-            group[i] = 1;
+        int gsize[] = new int [n];
+        int max = 0;
+        for(int i=0;i<n;i++){
             prev[i] = -1;
-            for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0) {
-                    if (group[i] < 1 + group[j]) {
-                        group[i] = 1 + group[j];
+            gsize[i] = 1;
+            for(int j=0;j<i;j++){
+                if(nums[i]%nums[j]==0){
+                    if(gsize[i]<1+gsize[j]){
+                        gsize[i] = 1 + gsize[j];
                         prev[i] = j;
                     }
                 }
             }
-            if (group[i] > group[maxIn])
-                maxIn = i;
+            if(gsize[max]<gsize[i]) max = i;
         }
-
         List<Integer> ans = new ArrayList<>();
-        while (maxIn != -1) {
-            ans.add(0, nums[maxIn]);
-            maxIn = prev[maxIn];
+        while(max!=-1){
+            ans.add(0 ,nums[max]);
+            max = prev[max];
         }
         return ans;
     }
