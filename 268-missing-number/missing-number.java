54e202412886e889a4e7014c4486305bc14990a7
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2026-01-29 12:47:15 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (76.98%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        HashSet<Integer> hs = new HashSet<>();
-        for(int a:nums) hs.add(a);
-        for(int i=0;i<=n;i++){
-            if(!hs.contains(i)) return i;
-        }
-        return 0;
+        int org = (n*(n+1))/2;
+        int sum = 0;
+        for(int i=0;i<n;i++) sum +=nums[i];
+        return org-sum;
     }
 }
