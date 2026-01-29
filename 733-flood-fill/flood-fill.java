736ed1ff1f3984529fcb275d4705adfb63c47a0a
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2026-01-30 01:14:32 +0530

    Time: 2 ms (12.58%) | Memory: 46.4 MB (98.11%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -7,39 +7,29 @@ class Solution {
         if (oldColor == color) return arr;
 
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] { sr, sc });
-        boolean vis[][] = new boolean[n][m];
-        vis[sr][sc] = true;
+        q.add(new int[]{sr, sc});
         arr[sr][sc] = color;
+
         while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int temp[] = q.remove();
-                int x = temp[0], y = temp[1];
+            int[] temp = q.remove();
+            int x = temp[0], y = temp[1];
 
-                if (x > 0 && arr[x - 1][y] == oldColor && !vis[x-1][y] ) {
-                    arr[x - 1][y] = color;
-                    q.add(new int[] { x - 1, y });
-                    vis[x-1][y] = true;
-                }
-                if (y > 0 && arr[x][y - 1] == oldColor && !vis[x][y-1] ) {
-                    arr[x][y - 1] = color;
-                    q.add(new int[] { x, y - 1 });
-                    vis[x][y-1] = true;
-                }
-                if (x < n - 1 && arr[x + 1][y] == oldColor && !vis[x+1][y] ) {
-                    arr[x + 1][y] = color;
-                    q.add(new int[] { x + 1, y });
-                    vis[x+1][y] = true;
-                }
-                if (y < m - 1 && arr[x][y + 1] == oldColor && !vis[x][y+1] ) {
-                    arr[x][y + 1] = color;
-                    q.add(new int[] { x, y + 1 });
-                    vis[x][y+1] = true;
-                }
-
+            if (x > 0 && arr[x-1][y] == oldColor) {
+                arr[x-1][y] = color;
+                q.add(new int[]{x-1, y});
             }
-             
+            if (y > 0 && arr[x][y-1] == oldColor) {
+                arr[x][y-1] = color;
+                q.add(new int[]{x, y-1});
+            }
+            if (x < n-1 && arr[x+1][y] == oldColor) {
+                arr[x+1][y] = color;
+                q.add(new int[]{x+1, y});
+            }
+            if (y < m-1 && arr[x][y+1] == oldColor) {
+                arr[x][y+1] = color;
+                q.add(new int[]{x, y+1});
+            }
         }
         return arr;
     }
