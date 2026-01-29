80d336bbb8572242db685d0b5af3785b129a17ea
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2026-01-29 12:50:23 +0530

    Time: 3 ms (58.66%) | Memory: 52.7 MB (17.77%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -3,9 +3,10 @@ class Solution {
         int n = nums.length;
         int max = 0, j = 0;
         for(int i=0;i<n;i++){
-            if(nums[i]==1) j++;
+            if(nums[i]==1){
+                max = Math.max(++j,max);
+            }
             else j = 0;
-            max = Math.max(j,max);
         }
         return max;
     }
