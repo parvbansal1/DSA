2a6035cfcf8bfc7764270f868b55c80190f99100
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2026-02-01 19:55:05 +0530

    Time: 1 ms (85.16%) | Memory: 43.4 MB (34.95%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -2,24 +2,15 @@ class Solution {
     public int numDecodings(String s) {
         int n = s.length();
         if (n == 0 || s.charAt(0) == '0') return 0;
-        int dp[] = new int[n+1];
-        Arrays.fill(dp, -1);
-        return find(0, s, dp);
-    }
-
-    public int find(int i, String s, int dp[]) {
-        int n = s.length();
-        if (i == n) return 1;
-        if(s.charAt(i)=='0') return 0;
-        
-        if(dp[i]!=-1) return dp[i];
-        int res = find(i+1, s, dp);
-        if(i+1<n){
-            int x = Integer.parseInt(s.substring(i,i+2));
-            if(x>=10 && x<=26){
-                res += find(i+2,s,dp);
-            }
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            int one = s.charAt(i - 1) - '0';
+            int two = Integer.parseInt(s.substring(i - 2, i));
+            if (one != 0)  dp[i] += dp[i - 1];
+            if (two >= 10 && two <= 26) dp[i] += dp[i - 2];
         }
-        return dp[i] = res;
+        return dp[n];
     }
 }
