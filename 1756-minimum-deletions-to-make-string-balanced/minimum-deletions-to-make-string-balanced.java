ea38365f79287c4432ab9f98b0e186ed8c209ef3
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2026-02-07 10:44:14 +0530

    Time: 133 ms (5.26%) | Memory: 48.2 MB (9.21%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,25 +1,18 @@
 class Solution {
+
     public int minimumDeletions(String s) {
         int n = s.length();
-        int arr1[] = new int[n];
-        int arr2[] = new int[n];
-        int b = 0, a = 0;
+        Stack<Character> st = new Stack<>();
+        int del = 0;
 
         for (int i = 0; i < n; i++) {
-            arr1[i] = b;
-            if (s.charAt(i) == 'b')
-                b++;
+            if (!st.isEmpty() && st.peek() == 'b' && s.charAt(i) == 'a') {
+                st.pop();
+                del++;
+            } else
+                st.push(s.charAt(i));
         }
 
-        for (int i = n - 1; i >= 0; i--) {
-            arr2[i] = a;
-            if (s.charAt(i) == 'a')
-                a++;
-        }
-        int min = n;
-        for (int i = 0; i < n; i++) {
-            min = Math.min(min, arr2[i] + arr1[i]);
-        }
-        return min;
+        return del;
     }
 }
