9eb7f32b74a804d6fa7d2e31e21cb06cdbd444ed
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2026-02-13 13:28:32 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (68.05%) - LeetSync

---
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
@@ -0,0 +1,8 @@
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root==null) return new TreeNode(val);
+        else if(val < root.val) root.left = insertIntoBST(root.left,val);   
+        else root.right = insertIntoBST(root.right, val);
+        return root;
+    }
+}
