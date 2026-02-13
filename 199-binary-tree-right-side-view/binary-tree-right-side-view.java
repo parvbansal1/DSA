70ca49c1272a8d1e180707cb0bc31d1bbcff9b61
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2026-02-13 22:54:44 +0530

    Time: 1 ms (72.37%) | Memory: 43.8 MB (29.84%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,25 +1,16 @@
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        if (root == null) return ans;
-        List<List<Integer>> adj = new ArrayList<>();
+        if(root==null) return ans;
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        while (!q.isEmpty()) {
-            List<Integer> list = new ArrayList<>();
-            int size = q.size();
-            for(int i=0;i<size;i++){
-            TreeNode temp = q.remove();
-            list.add(temp.val);
-            if(temp.left!=null) q.add(temp.left);
-            if(temp.right!=null) q.add(temp.right);
-            }
-            adj.add(list);
-        }
-        
-        for(int i=0;i<adj.size();i++){
-            for(int j=0;j<adj.get(i).size();j++){
-                if(j==adj.get(i).size()-1) ans.add(adj.get(i).get(j));
+        while(!q.isEmpty()){
+            int n = q.size();
+            for(int i=0;i<n;i++){
+                TreeNode temp = q.remove();
+                if(i==n-1) ans.add(temp.val);
+                if(temp.left!=null) q.add(temp.left);
+                if(temp.right!=null) q.add(temp.right);
             }
         }
         return ans;
