17e189c1c2d6e26a8c1c7db65eac350802c9aedd
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2026-02-23 11:07:08 +0530

    Time: 164 ms (53.58%) | Memory: 96.6 MB (56.30%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,29 +1,14 @@
 class Solution {
     public boolean hasAllCodes(String s, int k) {
         int n = s.length();
-        HashSet<String> hs = new HashSet<>();
-        for (int i = 0; i < n - k + 1; i++) {
-            hs.add(s.substring(i, i + k));
+        if (k > n) return false;
+
+        HashSet<String> set = new HashSet<>();
+        
+        for (int i = 0; i <= n - k; i++) {
+            set.add(s.substring(i, i + k));
         }
 
-        List<String> codes = binaryCodes(k);
-        for(int i=0; i<codes.size(); i++){
-            if(!hs.contains( codes.get(i)) ) return false;
-        }
-        return true;
-    }
-
-    public static List<String> binaryCodes(int nBits) {
-        List<String> arr = new ArrayList<>();
-        int maxNumber = 1 << nBits;
-        for (int i = 0; i < maxNumber; i++) {
-            String binary = Integer.toBinaryString(i);
-            
-            while (binary.length() < nBits) {
-                binary = "0" + binary;
-            }
-            arr.add(binary);
-        }
-        return arr;
+        return set.size() == (1 << k);
     }
 }
