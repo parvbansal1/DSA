82600b4e1588d138b6220e1abb388549fa36b72f
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2026-03-06 11:52:20 +0530

    Time: 13 ms (78.57%) | Memory: 47.2 MB (97.53%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+
         HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int i : nums){
-            hm.put(i, hm.getOrDefault(i,0) + 1);
+        for (int i : nums) hm.put(i, hm.getOrDefault(i, 0) + 1);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+
+        for (int key : hm.keySet()) {
+            pq.add(new int[] { key, hm.get(key) });
+            if (pq.size() > k) pq.remove();
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a,b) -> b[1] - a[1]
-        );
-        for(int i : nums){
-            if(hm.containsKey(i)){
-                pq.add(new int[]{i, hm.get(i)} );
-                hm.remove(i);
-            }
-        }
+
         int[] ans = new int[k];
-        for(int i=0;i<k;i++){
-            int temp[] = pq.remove();
-            ans[i] = temp[0];
+        int i = 0;
+        while (pq.size() > 0) {
+            ans[i++] = pq.remove()[0];
         }
         return ans;
     }
