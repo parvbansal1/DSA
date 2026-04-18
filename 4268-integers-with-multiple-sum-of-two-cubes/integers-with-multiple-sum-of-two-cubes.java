902fa9ecb9393416b9573e2ff617ba7cd584674b
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2026-04-18 18:02:57 +0530

    Time: 392 ms (19.09%) | Memory: 153.9 MB (91.58%) - LeetSync

--- 4268-integers-with-multiple-sum-of-two-cubes/integers-with-multiple-sum-of-two-cubes.java
+++ 4268-integers-with-multiple-sum-of-two-cubes/integers-with-multiple-sum-of-two-cubes.java
@@ -4,7 +4,11 @@ class Solution {
         HashMap<Integer,Integer> hm = new HashMap<>();
         for(int i=1;i*i*i<=n;i++){
             for(int j=i; (i*i*i + j*j*j ) <=n;j++){
-                hm.put(i*i*i + j*j*j,hm.getOrDefault(i*i*i + j*j*j,0)+1);
+                int sum = (int)(Math.pow(i,3) + Math.pow(j,3));
+                if(sum <= n){
+                    hm.put(sum,hm.getOrDefault(sum,0)+1);
+                }
+                else break;
             }
         }
         List<Integer> ans = new ArrayList<>();
