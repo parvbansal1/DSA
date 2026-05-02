046d9f0bc098678f6900b2667cf3cf2b0e9a3a05
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2026-05-02 15:46:38 +0530

    Time: 4 ms (59.28%) | Memory: 42.4 MB (27.60%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -1,22 +1,29 @@
 class Solution {
     public int rotatedDigits(int n) {
         int ans = 0;
+        int dp[] = new int[n + 1];
         for (int i = 0; i <= n; i++) {
-            int num = i;
-            boolean valid = false;
-            boolean bad = false;
-            while (num != 0) {
-                int d = num % 10;
-                if (d == 2 || d == 5 || d == 6 || d == 9)
-                    valid = true;
-                if (d == 3 || d == 4 || d == 7) {
-                    bad = true;
-                    break;
+            int d = i;
+            if (d < 10) {
+                if (d == 2 || d == 5 || d == 6 || d == 9) {
+                    dp[i] = 2;
+                    ans++;
+                } else if (d == 0 || d == 1 || d == 8) {
+                    dp[i] = 1;
+                } else
+                    dp[i] = 0;
+            } else {
+                int last = d % 10;
+                int rest = d / 10;
+                if (dp[last] == 0 || dp[rest] == 0) {
+                    dp[i] = 0;
+                } else if (dp[last] == 1 && dp[rest] == 1) {
+                    dp[i] = 1;
+                } else {
+                    dp[i] = 2;
+                    ans++;
                 }
-                num = num / 10;
             }
-            if (!bad && valid)
-                ans++;
         }
         return ans;
     }
