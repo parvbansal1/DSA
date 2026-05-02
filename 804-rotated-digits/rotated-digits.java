7c29d9f2a601567731758171dd835ee7cd4a65d2
Parv Bansal <139442981+parvbansal1@users.noreply.github.com>
2026-05-02 15:25:18 +0530

    Time: 5 ms (35.29%) | Memory: 41.9 MB (82.58%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -1,24 +1,22 @@
 class Solution {
     public int rotatedDigits(int n) {
-        HashSet<Integer> hs1 = new HashSet<>();
-        List<Integer> list = Arrays.asList(2,5,6,9);
-        hs1.addAll(list);
-        HashSet<Integer> hs2 = new HashSet<>();
-        List<Integer> list2 = Arrays.asList(3,4,7);
-        hs2.addAll(list2);
         int ans = 0;
         for (int i = 0; i <= n; i++) {
             int num = i;
             boolean valid = false;
-            while(num!=0){
-                int digit = num % 10;
-                if(hs1.contains(digit)) valid = true;
-                if(hs2.contains(digit)){
+            boolean bad = false;
+            while (num != 0) {
+                int d = num % 10;
+                if (d == 2 || d == 5 || d == 6 || d == 9)
+                    valid = true;
+                if (d == 3 || d == 4 || d == 7) {
+                    bad = true;
                     break;
-                } 
-                num = num/10;
-                if(num == 0 && valid) ans++;
+                }
+                num = num / 10;
             }
+            if (!bad && valid)
+                ans++;
         }
         return ans;
     }
